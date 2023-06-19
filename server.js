469ecf70f97ab6f7a846d22460b86a408d2a1927
Rohan Drummond <drummond.rohan@gmail.com>
2023-06-19 13:20:42 +1200

    connect-mongodb

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { MongoClient } = require('mongodb');
+require('dotenv').config()
+
+const mongosecret = process.env.MONGOSECRET
+const mongouri = `mongodb+srv://drummondrohan:${mongosecret}@arbie-abroad.fwjfcl6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(mongouri);
 
 app.listen(port, () => {
     console.log('App listening on port ' + port)
 })
 
 app.get('/', (req, res) => {
-    res.send("hello, world")
+    res.send('hello, world')
 })
