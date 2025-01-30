5583a081c2c18c24f58cbe0eb038b4d7b8ac464f
Rohan Drummond <drummond.rohan@gmail.com>
2025-01-30 12:56:43 +0700

    #89 Redirect users before Redux state is updated when users are logged out

--- client/src/Components/NavAuth.js
+++ client/src/Components/NavAuth.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 import { deAuthenticate } from '../redux/authenticator'
 
 function NavAuth() {
-
     const { authenticated } = useSelector((state) => state.authenticator);
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     async function handleLogout() {
         try {
+            navigate('/');
             dispatch(deAuthenticate());
         } catch(e) {
             console.error('Error logging out user: ', e)
         }
     }
-
     return (
         <>
             {
                 authenticated ? 
-                <a className='body-txt' onClick={handleLogout} href='/login'>Logout</a> :
+                <a className='body-txt' onClick={handleLogout}>Logout</a> :
                 <a className='body-txt' href='/login'>Account</a> 
             }
         </>
